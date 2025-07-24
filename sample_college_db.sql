a367c78ca6509bfc7734c3331cdb3ac747433cc0
Pranesh <praneshmadhan646@gmail.com>
2025-07-24 19:48:29 +0530

    Inserted values to student table

--- sample_college_db.sql
+++ sample_college_db.sql
@@ -5,3 +5,13 @@ marks INT NOT NULL,
 grade CHAR(1),
 city VARCHAR(15)
 );
+
+INSERT INTO
+student (id, name,marks,grade,city)
+VALUES 
+(1,'JS',100,'O','SNKL'),
+(2,'Pranesh',52,'C','PPY'),
+(3,'Suresh',80,'A','PPY'),
+(4,'Selva',82,'A','TUP'),
+(5,'Surya',90,'A','MDU'),
+(6,'HR',90,'A','THU');
